f34107da36bc254e089406e140feacc9b94be2ea
Tim <tim@brightsidedeveloper.com>
2023-05-13 16:15:24 -0500

    MOre changes

--- client/src/routes/appRoute.js
+++ client/src/routes/appRoute.js
@@ -1,9 +1,13 @@
+import AuthAPI from '../api/AuthAPI'
+
 /**
  * @returns {Promise<boolean>} Authenticated or not
  */
 export const appLoader = async () => {
   const token = localStorage.getItem('token')
 
+  const user = await AuthAPI.me()
+
   if (!token || token === 'undefined') return false
-  return true
+  return user
 }
